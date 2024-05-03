4e55c19f542db0bc11d52039973ad29d6c4e2a00
niyafrancis6 <niyachirayath@gmail.com>
2024-05-03 16:29:21 +0530

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+for (let n =1;n<=100;n++){
+    if(n%5==0 && n%3==0)
+    console.log("fizzbuzz")
+    if(n%3==0)
+    console.log("fizz")
+  if(n%5==0)
+console.log("buzz")
+  else
+console.log(n)
+}
---
+++ function.js
@@ -0,0 +1,7 @@
+function addnum(num1 =4,num2 =1){
+    console.log(num1+num2)
+}
+addnum(3,3)
+
+const addnum =(num1 =2,num2 =2) => num1 + num2
+console.log (addnum())
